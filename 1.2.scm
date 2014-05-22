e4aa46b121331a24def7d082d4c362c13d292fdd
Jacob Oscarson <jacob@plexical.com>
2014-05-22 18:05:27 +0200

    Completed 1.2

---
+++ 1.2.scm
@@ -0,0 +1,5 @@
+(/ (+ 5 4 (- 2 (- 3 (+ 6 (/ 4 5)))))
+   (* 3 (* (- 6 2) (- 2 7))))
+
+; Checked http://community.schemewiki.org/?sicp-ex-1.2:
+; [1/2 - unnecessary extra nested * in second part - evaluates correctly though
